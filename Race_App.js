bcb45a96a7ad7fa708160d93eda96350a4343aa5
Em Calderon <calderonhome@Ems-iMac.local>
2020-05-12 21:06:53 -0700

    color buttons can now change the color of car svg

---
+++ Race_App.js
@@ -0,0 +1,17 @@
+$(document).ready(()=> {
+    $("#red").click(()=> {
+        $(".fillcolor").css({ fill: "red" });
+    });
+    $("#yellow").click(()=> {
+        $(".fillcolor").css({ fill: "yellow" });
+    });
+    $("#green").click(()=> {
+        $(".fillcolor").css({ fill: "green" });
+    });
+    $("#blue").click(()=> {
+        $(".fillcolor").css({ fill: "blue" });
+    });
+    $("#purple").click(()=> {
+        $(".fillcolor").css({ fill: "purple" });
+    });
+});
