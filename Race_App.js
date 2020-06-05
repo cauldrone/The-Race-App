3757f0d7fd5e79cda0dd9627bcbe3af7d23ed59a
Em Calderon <calderonhome@Ems-iMac.local>
2020-06-05 14:33:44 -0700

    last commit

--- Race_App.js
+++ Race_App.js
@@ -1,6 +1,6 @@
 $(document).ready(()=> {
 
-    //change color of the car on menu page
+    //change color of the car on menu page (works!)
     $("#red").click(()=> {
         $(".fillcolor").css({ fill: "red" });
     });
@@ -17,63 +17,142 @@ $(document).ready(()=> {
         $(".fillcolor").css({ fill: "purple" });
     });
 
-    //change color of game car
-    //function carColor (){}
+    //change color of game car (not working)
+    function carColor (color){
+        if ($(".fillcolor").css({ fill: "red" })) {
+             $('#fillmini').css({fill: "red"});
+        }
+        if ($(".fillcolor").css({ fill: "yellow" })) {
+             $('#fillmini').css({fill: "yellow"});
+        }
+        if ($(".fillcolor").css({ fill: "green" })) {
+             $('#fillmini').css({fill: "green"});
+        }
+        if ($(".fillcolor").css({ fill: "blue" })) {
+             $('#fillmini').css({fill: "blue"});
+        }
+        if ($(".fillcolor").css({ fill: "purple" })) {
+             $('#fillmini').css({fill: "purple"});
+        }
+    };
 
-    //move car (left, right, up, down)
-    $('body').keyup(function(e){
+    //finishline
+    let $target = $("#final");
+    let $source = $("#car");
+
+    let $popup = $("#backgroundbox");
+    let $game = $("#levelone");
+
+    function testCollision(s, t) {
+    if (
+    s.position().left < t.position().left + t.width() &&
+    s.position().left + s.width() > t.position().left &&
+    s.position().top < t.position().top + t.height() &&
+    s.position().top + s.height() > t.position().top
+    ) {
+    console.log("collision!");
+    return true;
+    }
+    console.log("no collision");
+    return false;
+    }
+
+
+
+
+    //move car (left, right, up, down) (works!!)
+    $("body").keyup((event) => function(e){
+
         //left
         if(e.keyCode == 37){
-        $("#car").animate({
+        $source.animate({
             marginLeft: '-=55px'
             });
         }
         //right
         if(e.keyCode == 39){
-        $("#car").animate({
+        $source.animate({
             marginLeft: '+=55px'
             });
         }
         //up
         if(e.keyCode == 38){
-        $("#car").animate({
-            marginTop: '-=55px'
+        $source.animate({
+            marginTop: '-=50px'
             });
         }
         //down
         if(e.keyCode == 40){
-        $("#car").animate({
+        $source.animate({
             marginTop: '+=55px'
             });
         }
+
+        //if (testCollision($source, $target) == true) {
+        //  $game.hide();
+         // $popup.show();
+       // } else {
+        //  $game.show();
+        //  $popup.hide();
+       // }
+
     });
 
-    //obstacles
+    //obstacles (none unfortunately)
 
-    //stopwatch
+
+    //stopwatch (works!!)
+    let seconds = 0;
+    let minutes = 0;
+
+    let displaySeconds = 0;
+    let displayMinutes = 0;
+
+    let interval;
+
+    let timer = $("#timer");
+
     function stopwatch () {
-        var milliseconds = 0;
-        var seconds = 0;
-        var minutes = 0;
-        document.getElementById("timer").innerHTML = minutes + ":" + seconds + ":" + milliseconds;
+
+        seconds++;
+        if (seconds/60 ===1) {
+            seconds = 0;
+            minutes++;
+        }
+
+        if (seconds<10){
+            displaySeconds = "0" + seconds.toString();
+        }else {
+            displaySeconds = seconds;
+        }
+
+        if (minutes < 10){
+            displayMinutes ="0" + minutes.toString();
+        }else{
+            displayMinutes = minutes;
+        }
+        document.getElementById("timer").innerHTML = displayMinutes + ":" + displaySeconds;
     }
 
-    //start button on menu screen
+
+    //start button on menu screen (works!!)
     let $menupage = $("#menubox");
     let $startbutton = $("#startbutton");
     $startbutton.on({
         click: () => {
             $menupage.hide();
-            stopwatch();
             carColor();
+            clearInterval(interval);
+            interval = setInterval(stopwatch, 1000);
         }
     });
 
-    //menu button on game screen
+    //menu button on game screen (kinda works!!)
     let $menubutton = $("#menubutton");
     $menubutton.on({
         click: () => {
             $menupage.show();
+            //restart stopwatch (does not work)
         }
     });
 
