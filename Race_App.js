bbc8efda6725341f78109dbe43bd99e9e59778f5
cauldrone <56094737+cauldrone@users.noreply.github.com>
2020-06-05 17:46:50 -0700

    deleted the carColor () function because it is unnecessary

--- Race_App.js
+++ Race_App.js
@@ -3,40 +3,27 @@ $(document).ready(()=> {
     //change color of the car on menu page (works!)
     $("#red").click(()=> {
         $(".fillcolor").css({ fill: "red" });
+        $('#fillmini').css({fill: "red"});
     });
     $("#yellow").click(()=> {
         $(".fillcolor").css({ fill: "yellow" });
+        $('#fillmini').css({fill: "yellow"});
     });
     $("#green").click(()=> {
         $(".fillcolor").css({ fill: "green" });
+        $('#fillmini').css({fill: "green"});
     });
     $("#blue").click(()=> {
         $(".fillcolor").css({ fill: "blue" });
+        $('#fillmini').css({fill: "blue"});
     });
     $("#purple").click(()=> {
         $(".fillcolor").css({ fill: "purple" });
+        $('#fillmini').css({fill: "purple"});
     });
 
-    //change color of game car (not working)
-    function carColor (color){
-        if ($(".fillcolor").css({ fill: "red" })) {
-             $('#fillmini').css({fill: "red"});
-        }
-        if ($(".fillcolor").css({ fill: "yellow" })) {
-             $('#fillmini').css({fill: "yellow"});
-        }
-        if ($(".fillcolor").css({ fill: "green" })) {
-             $('#fillmini').css({fill: "green"});
-        }
-        if ($(".fillcolor").css({ fill: "blue" })) {
-             $('#fillmini').css({fill: "blue"});
-        }
-        if ($(".fillcolor").css({ fill: "purple" })) {
-             $('#fillmini').css({fill: "purple"});
-        }
-    };
 
-    //finishline
+    //finishline (does not work)
     let $target = $("#final");
     let $source = $("#car");
 
@@ -141,7 +128,6 @@ $(document).ready(()=> {
     $startbutton.on({
         click: () => {
             $menupage.hide();
-            carColor();
             clearInterval(interval);
             interval = setInterval(stopwatch, 1000);
         }
